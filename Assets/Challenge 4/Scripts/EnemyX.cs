d47ca3002a5521d5b9d24106c185e7556fbcb631
MuhammetKaval <muhammetkaval@yahoo.com>
2022-04-23 11:22:58 +0300

    Challenge 4 - Done
    Challenge 4 - Soccer Scripting

---
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed = 50;
+    private Rigidbody enemyRb;
+    public GameObject playerGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerGoal = GameObject.Find("Player Goal");
+        enemyRb = GetComponent<Rigidbody>();
+        //speed = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+
+    }
+
+    //public void IncreaseEnemySpeedEveryWaves()
+    //{
+    //    speed += 5;
+    //}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
